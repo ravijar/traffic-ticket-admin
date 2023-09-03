6f638ec9fd10eca5c97f63ffcf33da1729f3d40f
ravijar <ravijarandinu@gmail.com>
2023-09-03 14:12:15 +0530

    added form actions

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Box,
   Card,
@@ -10,9 +11,81 @@ import {
   Chip,
   Button,
 } from "@mui/material";
-import React from "react";
 
 const Register = () => {
+  // text field values
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [nic, setNic] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [officerId, setOfficerId] = useState("");
+  const [policeStation, setPoliceStation] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  // text field errors
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [nicError, setNicError] = useState(false);
+  const [telephoneError, setTelephoneError] = useState(false);
+  const [officerIdError, setOfficerIdError] = useState(false);
+  const [policeStationError, setPoliceStationError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // checking for empty text fields
+    if (firstName === "") {
+      setFirstNameError(true);
+    }
+    if (lastName === "") {
+      setLastNameError(true);
+    }
+    if (nic === "") {
+      setNicError(true);
+    }
+    if (telephone === "") {
+      setTelephoneError(true);
+    }
+    if (officerId === "") {
+      setOfficerIdError(true);
+    }
+    if (policeStation === "") {
+      setPoliceStationError(true);
+    }
+    if (password === "") {
+      setPasswordError(true);
+    }
+    if (confirmPassword === "") {
+      setConfirmPasswordError(true);
+    }
+
+    // submit action
+    if (
+      firstName &&
+      lastName &&
+      nic &&
+      telephone &&
+      officerId &&
+      policeStation &&
+      password &&
+      confirmPassword
+    ) {
+      console.log(
+        firstName,
+        lastName,
+        nic,
+        telephone,
+        officerId,
+        policeStation,
+        password,
+        confirmPassword
+      );
+    }
+  };
+
   return (
     <Container>
       <Grid container spacing={6}>
@@ -22,7 +95,7 @@ const Register = () => {
             <Container>
               <CardHeader title="Officer Details" />
               <CardContent>
-                <form>
+                <form noValidate autoComplete="off">
                   {/* personal details */}
                   <Divider sx={{ marginBottom: 4 }}>
                     <Chip label="Personal Details" />
@@ -30,31 +103,51 @@ const Register = () => {
                   <Box sx={{ display: "flex" }}>
                     <TextField
                       sx={{ marginBottom: 3, marginRight: 1 }}
+                      onChange={(e) => {
+                        setFirstName(e.target.value);
+                        setFirstNameError(false);
+                      }}
                       label="First Name"
                       variant="outlined"
                       fullWidth
                       required
+                      error={firstNameError}
                     />
                     <TextField
                       sx={{ marginBottom: 3, marginLeft: 1 }}
+                      onChange={(e) => {
+                        setLastName(e.target.value);
+                        setLastNameError(false);
+                      }}
                       label="Last Name"
                       variant="outlined"
                       fullWidth
                       required
+                      error={lastNameError}
                     />
                   </Box>
                   <TextField
                     sx={{ marginBottom: 3 }}
+                    onChange={(e) => {
+                      setNic(e.target.value);
+                      setNicError(false);
+                    }}
                     label="NIC"
                     variant="outlined"
                     fullWidth
                     required
+                    error={nicError}
                   />
                   <TextField
                     sx={{ marginBottom: 4, width: "50%" }}
+                    onChange={(e) => {
+                      setTelephone(e.target.value);
+                      setTelephoneError(false);
+                    }}
                     label="Telephone"
                     variant="outlined"
                     required
+                    error={telephoneError}
                   />
                   {/* official details */}
                   <Divider sx={{ marginBottom: 4 }}>
@@ -63,17 +156,27 @@ const Register = () => {
                   <Box sx={{ display: "flex" }}>
                     <TextField
                       sx={{ marginBottom: 4, marginRight: 1 }}
+                      onChange={(e) => {
+                        setOfficerId(e.target.value);
+                        setOfficerIdError(false);
+                      }}
                       label="Officer ID"
                       variant="outlined"
                       fullWidth
                       required
+                      error={officerIdError}
                     />
                     <TextField
                       sx={{ marginBottom: 4, marginLeft: 1 }}
+                      onChange={(e) => {
+                        setPoliceStation(e.target.value);
+                        setPoliceStationError(false);
+                      }}
                       label="Police Station"
                       variant="outlined"
                       fullWidth
                       required
+                      error={policeStationError}
                     />
                   </Box>
                 </form>
@@ -87,23 +190,34 @@ const Register = () => {
               {/* credentials */}
               <CardHeader title="Credentials" />
               <CardContent>
-                <form>
+                <form onSubmit={handleSubmit} noValidate autoComplete="off">
                   <TextField
                     sx={{ marginBottom: 3 }}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      setPasswordError(false);
+                    }}
                     label="Password"
                     variant="outlined"
                     fullWidth
                     required
+                    error={passwordError}
                   />
                   <TextField
                     sx={{ marginBottom: 4 }}
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                      setConfirmPasswordError(false);
+                    }}
                     label="Confirm Password"
                     variant="outlined"
                     fullWidth
                     required
+                    error={confirmPasswordError}
                   />
                   <Button
                     variant="contained"
+                    type="submit"
                     fullWidth
                     size="large"
                     sx={{ marginBottom: 4 }}
