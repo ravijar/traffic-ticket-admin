9d087c4f0d9f06fcb1e40696671b98332c71363d
ravijar <ravijarandinu@gmail.com>
2023-09-12 01:16:15 +0530

    added background color

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,89 +28,100 @@ const Login = () => {
   const theme = useTheme();
 
   return (
-    <Container
-      maxWidth="sm"
-      disableGutters
+    <Box
       sx={{
-        bgcolor: "white",
-        borderTopRightRadius: 15,
-        borderBottomRightRadius: 15,
-        borderTopLeftRadius: 15,
-        borderBottomLeftRadius: 15,
-        boxShadow: 5,
+        background: "#f2f2f2",
+        flexGrow: 1,
+        height: "100%",
+        minHeight: "100vh",
+        boxSizing: "border-box",
+        padding: (theme) => theme.spacing(2),
       }}
     >
-      <Grid container spacing={0} marginTop="20vh">
-        {/* logo panel */}
-        <Grid item xs={3} md={5} justifyContent="center">
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="50vh"
-            sx={{
-              bgcolor: theme.palette.primary.main,
-              borderTopLeftRadius: 15,
-              borderBottomLeftRadius: 15,
-            }}
-          >
-            <img src="./logo.png" width="60%" alt="logo" />
-          </Box>
-        </Grid>
-
-        {/* login form */}
-        <Grid item xs={9} md={7} justifyContent="center">
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="50vh"
-            paddingLeft={5}
-            paddingRight={5}
-          >
-            <TextField
-              label="User ID"
-              fullWidth
-              variant="outlined"
-              sx={{ m: 1 }}
-              size="small"
-            />
-            <FormControl
-              variant="outlined"
-              fullWidth
-              sx={{ m: 1 }}
-              size="small"
+      <Container
+        maxWidth="sm"
+        disableGutters
+        sx={{
+          bgcolor: "white",
+          borderTopRightRadius: 15,
+          borderBottomRightRadius: 15,
+          borderTopLeftRadius: 15,
+          borderBottomLeftRadius: 15,
+          boxShadow: 5,
+        }}
+      >
+        <Grid container spacing={0} marginTop="20vh">
+          {/* logo panel */}
+          <Grid item xs={3} md={5} justifyContent="center">
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              minHeight="50vh"
+              sx={{
+                bgcolor: theme.palette.primary.main,
+                borderTopLeftRadius: 15,
+                borderBottomLeftRadius: 15,
+              }}
             >
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
+              <img src="./logo.png" width="60%" alt="logo" />
+            </Box>
+          </Grid>
+
+          {/* login form */}
+          <Grid item xs={9} md={7} justifyContent="center">
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              minHeight="50vh"
+              paddingLeft={5}
+              paddingRight={5}
+            >
+              <TextField
+                label="User ID"
+                fullWidth
+                variant="outlined"
+                sx={{ m: 1 }}
+                size="small"
               />
-            </FormControl>
-            <Button fullWidth variant="contained" sx={{ m: 1 }}>
-              Login
-            </Button>
-          </Box>
+              <FormControl
+                variant="outlined"
+                fullWidth
+                sx={{ m: 1 }}
+                size="small"
+              >
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button fullWidth variant="contained" sx={{ m: 1 }}>
+                Login
+              </Button>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
