995617da9daa18cf7962bd30a2d3d39fe481a75a
ravijar <ravijarandinu@gmail.com>
2023-10-11 21:52:53 +0530

    added mock implementations for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,25 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// mock implementations to avoid errors
+jest.mock("@mui/x-charts/PieChart", () => ({
+  PieChart: jest.fn().mockImplementation(({ children }) => children),
+}));
+
+jest.mock("@mui/x-charts", () => ({
+  LineChart: jest.fn().mockImplementation(({ children }) => children),
+}));
+jest.mock("@mui/x-date-pickers", () => ({
+  DatePicker: jest.fn().mockImplementation(({ children }) => children),
+  LocalizationProvider: jest
+    .fn()
+    .mockImplementation(({ children }) => children),
+}));
+jest.mock("@mui/x-date-pickers/AdapterDayjs", () => ({
+  AdapterDayjs: jest.fn().mockImplementation(({ children }) => children),
+}));
+jest.mock("@mui/x-date-pickers/internals/demo", () => ({
+  DemoContainer: jest.fn().mockImplementation(({ children }) => children),
+}));
