8d898a986e1e7c67c67cd36e824567d0a9f6416f
ravijar <ravijarandinu@gmail.com>
2023-10-31 01:14:14 +0530

    api url bug fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -62,7 +62,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   let updateToken = async () => {
-    let response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+    let response = await fetch(`${API_URL}/api/token/refresh/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
